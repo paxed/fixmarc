81c3a7e5fb25a146130262c88f2bf490a61bc813
Pasi Kallinen <paxed@alt.org>
2019-11-01 13:13:12 +0200

    Fix 008/33 space

---
+++ fix_008_33_space.pl
@@ -0,0 +1,36 @@
+use strict;
+use warnings;
+
+do './FixMarc.pm';
+use MARC::Record;
+use MARC::Field;
+
+sub str_replace_nth {
+    my ($str, $nth, $chr) = @_;
+    return substr($str, 0, $nth).$chr.substr($str,($nth + length($chr))-length($str));
+}
+
+sub fix_008 {
+    my ($fixer, $record) = @_;
+
+    my @flist = $record->field('008');
+
+    foreach my $f (@flist) {
+        my $fdata = $f->data();
+        my $ofdata = $fdata;
+
+        my $p33 = substr($fdata, 33, 1);
+
+        if ($p33 eq ' ') {
+                $fdata = str_replace_nth($fdata, 33, '|');
+                $f->update($fdata);
+                $fixer->msg("Changed 008 '".$ofdata."'=>'".$fdata."'");
+        }
+    }
+}
+
+my $fixer = FixMarc->new({
+    'where' => 'substr(ExtractValue(metadata, \'//controlfield[@tag="008"]\'), 33, 1) = " "',
+    'func' => \&fix_008
+                         });
+$fixer->run();
