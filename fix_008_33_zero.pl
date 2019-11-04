b0f57b2a38ac4e37aa6d3760cf7d1559cdacc83a
Pasi Kallinen <paxed@alt.org>
2019-11-04 10:03:59 +0200

    Fix 008/33 zero

---
+++ fix_008_33_zero.pl
@@ -0,0 +1,41 @@
+use strict;
+use warnings;
+
+do './FixMarc.pm';
+use MARC::Record;
+use MARC::Field;
+
+sub str_replace_nth {
+    my ($str, $nth, $chr) = @_;
+    return substr($str, 0, $nth).$chr.substr($str,($nth + length($chr))-length($str));
+}
+
+sub fix_008 {
+    my ($fixer, $record) = @_;
+
+    my $ldr67 = substr($record->leader(), 6, 2);
+
+    # ldr/6 == a OR t, AND ldr/7 == b OR i OR s
+    return if (!($ldr67 =~ /^[at][bis]$/));
+
+    my @flist = $record->field('008');
+
+    foreach my $f (@flist) {
+        my $fdata = $f->data();
+        my $ofdata = $fdata;
+
+        my $p33 = substr($fdata, 33, 1);
+
+        if ($p33 eq '0') {
+                $fdata = str_replace_nth($fdata, 33, '|');
+                $f->update($fdata);
+                $fixer->msg("Changed 008 ($ldr67) '".$ofdata."'=>'".$fdata."'");
+        }
+    }
+}
+
+my $fixer = FixMarc->new({
+    'where' => 'substr(ExtractValue(metadata, \'//controlfield[@tag="008"]\'), 34, 1) = "0"',
+    'func' => \&fix_008
+                         });
+$fixer->run();
