692dbb7309178fb4c244d1315c92b83af78889b5
Pasi Kallinen <pasi.kallinen@koha-suomi.fi>
2019-11-06 12:32:34 +0200

    Better 245 ind 2 skip value

--- fix_245ind2.pl
+++ fix_245ind2.pl
@@ -13,8 +13,26 @@ sub fix_245_ind2 {
     foreach my $f (@flist) {
         my $ind = $f->indicator(2) . "";
         if ($ind !~ /^[0-9]$/) {
-            $f->set_indicator(2, "0");
-            $fixer->msg("Changed 245.ind2:'".$ind."'=>'0'");
+            my $s = $f->subfield('a');
+            if ($s) {
+                my $lang = '';
+                my $f008 = $record->field('008');
+                if (defined($f008)) {
+                    my $f008data = $f008->data();
+                    if (length($f008data) == 40) {
+                        $lang = lc(substr($f008data, 35, 3));
+                    }
+                }
+
+                my $ival = 0;
+                if ($s =~ /^(The |An |A |L'|La |Les |El |Il |Un |Det |Den |Ett )/i) {
+                    $ival = length($1);
+                } elsif ($lang eq 'ger' && $s =~ /^(Der |Die |Das )/i) {
+                    $ival = length($1);
+                }
+                $f->set_indicator(2, "".$ival);
+                $fixer->msg("Changed 245.ind2:'".$ind."'=>'".$ival."'");
+            }
         }
     }
 }
